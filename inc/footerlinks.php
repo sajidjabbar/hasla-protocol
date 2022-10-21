c17c51cdf72f90b44590af9b7c57222b4ff2f240
zainahmed768 <zainahmed231@gmail.com>
2022-10-21 20:28:46 +0500

    first commit

---
+++ inc/footerlinks.php
@@ -0,0 +1,13 @@
+<!-- Jquery JS-->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" type="text/javascript"></script>
+<!-- Jquery JS -->
+<!-- bootstrap starts here -->
+<script src="js/bootstrap.min.js"></script>
+<!-- bootstrap ends here -->
+<script>
+    $(document).ready(function() {
+        $(window).on('load', function() {
+            $("#preloader").fadeOut(1000);
+        });
+    })
+</script>
