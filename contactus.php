94f324d6df1ee2ff2287bb2d3246b37fbc9761a2
zainahmed768 <mzainahmedr231@gmail.com>
2022-10-26 09:10:07 -0700

    Contact us page contact form

---
+++ contactus.php
@@ -0,0 +1,106 @@
+<?php include 'inc/app.php'; ?>
+<!-- css starts here -->
+<link rel="stylesheet" href="css/contact-us.css">
+<!-- css ends here -->
+<!-- page Header starts here -->
+<section class="pageHeader-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="page-header-wrapper">
+                    <div class="heading-wrapper">
+                        <h3>Contact us</h3>
+                    </div>
+                    <div class="breadcrumbs-wrapper">
+                        <ul>
+                            <li class="base-page"><a href="#">HOME</a></li>
+                            <li class="arrow"><a href="#">></a></li>
+                            <li class="current-page"><a href="#">Contact us</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- page Header ends here -->
+<!-- contact us starts here -->
+<section class="contact-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-8">
+                <div class="content-wrapper">
+                    <h2>contact information</h2>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.Lorem Ipsum is simply dummy text of the printing and
+                    </p>
+                    <div class="social-media-icon-wrapper">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-instagram"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-linkedin"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-youtube"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2"></div>
+        </div>
+        <div class="row">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-8">
+                <div class="form-wrapper">
+                    <form action="">
+                        <div class="form-group row">
+                            <div class="col-lg-6">
+                                <input type="text" class="form-control" placeholder="First Name">
+                            </div>
+                            <div class="col-lg-6">
+                                <input type="text" class="form-control" placeholder="Last Name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="email" name="" class="form-control" placeholder="Email" id="">
+                        </div>
+                        <div class="form-group">
+                            <textarea name="" placeholder="Message" class="form-control" id="" cols="30" rows="6"></textarea>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-lg-8">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                    <label class="form-check-label" for="flexCheckDefault">
+                                        Accept Terms And Conditions
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 d-flex justify-content-end">
+                                <button type="submit" class="btn">Submit <i class="fa fa-arrow-right"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-2"></div>
+        </div>
+    </div>
+</section>
+<!-- contact us ends here -->
+<?php include 'inc/footer.php'; ?>
