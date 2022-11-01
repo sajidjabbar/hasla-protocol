77548104b7272257307839fa0625bfba1b276f80
zainahmed768 <mzainahmedr231@gmail.com>
2022-11-01 13:02:14 -0700

    2 more pages

---
+++ exchange-pay.php
@@ -0,0 +1,178 @@
+<!-- App Include -->
+<?php include 'inc/app.php' ?>
+<!-- App Include -->
+
+<!-- Stylesheet -->
+<link rel="stylesheet" href="css/exchange-dashboard.css">
+<link rel="stylesheet" href="css/exchange-pay.css">
+<!-- Stylesheet -->
+
+<!-- page Header starts here -->
+<section class="pageHeader-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="page-header-wrapper">
+                    <div class="heading-wrapper">
+                        <h3>EXCHANGE</h3>
+                    </div>
+                    <div class="breadcrumbs-wrapper">
+                        <ul>
+                            <li class="base-page"><a href="#">HOME</a></li>
+                            <li class="arrow"><a href="#">></a></li>
+                            <li class="current-page"><a href="#">EXCHANGE</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- page Header ends here -->
+
+<!-- Exchange Assets Sec Start Here -->
+<section class="exchange_dashboard">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3"></div>
+            <div class="col-lg-9">
+                <div class="dashboard">
+                    <div class="topbar">
+                        <div class="heading">
+                            <h3>Pay</h3>
+                        </div>
+                        <div class="right-side">
+                            <div class="button-group">
+                                <button>Buy & Sell
+                                    <span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17.546" viewBox="0 0 18 17.546">
+                                            <path id="Icon_awesome-arrow-right" data-name="Icon awesome-arrow-right" d="M7.653,3.818l.892-.892a.96.96,0,0,1,1.362,0l7.809,7.805a.96.96,0,0,1,0,1.362l-7.81,7.812a.96.96,0,0,1-1.362,0l-.891-.89a.965.965,0,0,1,.016-1.378l4.843-4.614H.964A.962.962,0,0,1,0,12.06V10.775a.962.962,0,0,1,.964-.964H12.513L7.669,5.2a.958.958,0,0,1-.016-1.38Z" transform="translate(0 -2.643)" fill="#fff" />
+                                        </svg>
+                                    </span>
+                                </button>
+                                <button>Send & Recieve
+                                    <span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17.546" viewBox="0 0 18 17.546">
+                                            <path id="Icon_awesome-arrow-right" data-name="Icon awesome-arrow-right" d="M7.653,3.818l.892-.892a.96.96,0,0,1,1.362,0l7.809,7.805a.96.96,0,0,1,0,1.362l-7.81,7.812a.96.96,0,0,1-1.362,0l-.891-.89a.965.965,0,0,1,.016-1.378l4.843-4.614H.964A.962.962,0,0,1,0,12.06V10.775a.962.962,0,0,1,.964-.964H12.513L7.669,5.2a.958.958,0,0,1-.016-1.38Z" transform="translate(0 -2.643)" fill="#fff" />
+                                        </svg>
+                                    </span>
+                                </button>
+                            </div>
+                            <div class="right-side">
+                                <div class="filters">
+                                    <button>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20.038" height="21.856" viewBox="0 0 20.038 21.856">
+                                            <g id="Group_789" data-name="Group 789" transform="translate(-2461.142 1255.89)">
+                                                <path id="Path_819" data-name="Path 819" d="M2721.61-1255.89c.344.082.7.138,1.029.25a4.544,4.544,0,0,1,3.061,4.766,4.553,4.553,0,0,1-3.9,4.041,4.552,4.552,0,0,1-5.132-3.8,4.554,4.554,0,0,1,3.673-5.183c.124-.022.247-.052.37-.078Z" transform="translate(-244.543 0)" fill="#fff" />
+                                                <path id="Path_820" data-name="Path 820" d="M2470.842-1213.176a6.249,6.249,0,0,0,.425,6.118,6.279,6.279,0,0,0,5.332,2.961c.078.449.127.9.237,1.338a5.8,5.8,0,0,0,1.866,3.039,1.6,1.6,0,0,1,.6,1.707,1.654,1.654,0,0,1-1.595,1.188c-.064,0-.128,0-.192,0q-7.265,0-14.529,0a1.977,1.977,0,0,1-1.153-.314,1.551,1.551,0,0,1-.147-2.468,6.457,6.457,0,0,0,1.717-2.331,5.9,5.9,0,0,0,.475-2.238c.01-.875,0-1.749,0-2.624a6.362,6.362,0,0,1,4.734-6.2,6.4,6.4,0,0,1,2.113-.2C2470.759-1213.193,2470.792-1213.184,2470.842-1213.176Z" transform="translate(0 -40.855)" fill="#fff" />
+                                                <path id="Path_821" data-name="Path 821" d="M2596.27-808.621h6.642c.013,1.208-1.515,2.575-3.032,2.716A3.424,3.424,0,0,1,2596.27-808.621Z" transform="translate(-129.349 -428.143)" fill="#fff" />
+                                            </g>
+                                        </svg>
+                                    </button>
+                                    <button>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="19.392" height="19.602" viewBox="0 0 19.392 19.602">
+                                            <path id="Path_818" data-name="Path 818" d="M2268.625-206.1a1,1,0,0,1,.629.541.917.917,0,0,1-.205,1.04c-.618.662-1.229,1.331-1.843,2q-2.575,2.793-5.152,5.585a1.467,1.467,0,0,0-.41,1.054c.008,1.71,0,3.419.008,5.129a1.538,1.538,0,0,1-.395,1.08c-.855.972-1.7,1.953-2.552,2.929a.593.593,0,0,1-.683.21.593.593,0,0,1-.411-.565c-.006-.07,0-.14,0-.21q0-4.287,0-8.574a1.45,1.45,0,0,0-.4-1.04c-2.3-2.493-4.588-5-6.913-7.475a.977.977,0,0,1,.328-1.7Z" transform="translate(-2249.943 206.095)" fill="#fff" />
+                                        </svg>
+                                    </button>
+                                </div>
+                                <div class="profile">
+                                    <button>
+                                        <img src="img/profile.png" alt="" class="img-fluid">
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3">
+                <?php include 'inc/dashboard-sidebar.php' ?>
+            </div>
+            <div class="col-lg-6">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Send</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Receive </button>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        <div class="amount-send-wrapper">
+                            <div class="wallet-amount">
+                                <h6>$ <span>0</span></h6>
+                            </div>
+                            <div class="send-all">
+                                <div class="btn-wrapper">
+                                    <button class="btn">Send All</button>
+                                </div>
+                                <div class="btc-wrapper">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300">
+                                        <text id="Send_All" data-name="Send All" fill="#707070" font-size="20" font-family="SegoeUI, Segoe UI">
+                                            <tspan x="0" y="22">Send All</tspan>
+                                        </text>
+                                    </svg>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="pay-side-wrapper">
+                    <div class="gift-card-wrapper">
+                        <div class="gift-img-wrapper">
+                            <figure>
+                                <img src="img/side-art.png" class="img-fluid" alt="">
+                            </figure>
+                        </div>
+                        <div class="gift-content-wrapper">
+                            <h5>Crypto Gift</h5>
+                            <p>
+                                Give Crypto To Your Friends And Family
+                            </p>
+                            <a href="#">Get Started <i class="fa fa-angle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="recent-send">
+                        <h4>Recent Send </h4>
+                        <p>
+                            Easily send to recent destination
+                        </p>
+                    </div>
+                    <div class="wallet-details">
+                        <div class="icon-wrapper">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="39" height="39" viewBox="0 0 39 39">
+                                <defs>
+                                    <linearGradient id="linear-gradient" x1="0.146" y1="0.055" x2="1" y2="1" gradientUnits="objectBoundingBox">
+                                        <stop offset="0" stop-color="#2bdeff" />
+                                        <stop offset="1" stop-color="#01667a" />
+                                    </linearGradient>
+                                </defs>
+                                <g id="Group_931" data-name="Group 931" transform="translate(-1412 -1008)">
+                                    <circle id="Ellipse_139" data-name="Ellipse 139" cx="19.5" cy="19.5" r="19.5" transform="translate(1412 1008)" fill="url(#linear-gradient)" />
+                                    <path id="Icon_awesome-wallet" data-name="Icon awesome-wallet" d="M13.671,5.1H2.371a.474.474,0,1,1,0-.949H13.754a.474.474,0,0,0,.474-.474A1.423,1.423,0,0,0,12.806,2.25H1.9A1.9,1.9,0,0,0,0,4.147v9.486a1.9,1.9,0,0,0,1.9,1.9H13.671a1.468,1.468,0,0,0,1.506-1.423V6.519A1.468,1.468,0,0,0,13.671,5.1Zm-1.34,6.166a.949.949,0,1,1,.949-.949A.949.949,0,0,1,12.331,11.261Z" transform="translate(1423.911 1018.61)" fill="#fff" />
+                                </g>
+                            </svg>
+
+                        </div>
+                        <div class="wallet-des-wrapper">
+                            <h6>21465Xjgfvsgdc465csa4c65sa46....</h6>
+                            <span>Sent BTC on mar 28 2022</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Exchange Assets Sec End Here -->
+
+<!-- Footer Include -->
+<?php include 'inc/footer.php' ?>
+<!-- Footer Include -->
